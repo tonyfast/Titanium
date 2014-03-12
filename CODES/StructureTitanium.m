b8b7472d4c6604e3aba8799168935c8bb65bc294
System Administrator <root@ipsec-143-215-23-105.vpn.gatech.edu>
2014-03-12 12:25:42 -0400

    modified Translater function

--- CODES/StructureTitanium.m
+++ CODES/StructureTitanium.m
@@ -2,14 +2,24 @@ function A = StructureTitanium( ImageLocation )
 
 [~,A.name ] = fileparts( ImageLocation);
 
-A.spatial.phase = round(imread( ImageLocation )./255);
+A.spatial.phase = double(round(imread( ImageLocation )./255));
 [~,imnm,ext] = fileparts(ImageLocation);
 
-A.image = flickrGetImage( 'flickr.photos.search','Medium 640','text', sprintf('"%s"',imnm));   
-A.link = flickrGetImage( 'flickr.photos.search','Original','text', sprintf('"%s"',imnm));
+shifts = [ eye(2) ];
+A.spatial.edge = zeros( size(A.spatial.phase));
 
-A.spatial.edge = double(edge( A.spatial.phase ));
+for ii = 1 : size(shifts,2)
+A.spatial.edge(:) = or(A.spatial.edge, abs(A.spatial.phase + circshift(A.spatial.edge, shifts(ii,:))));
+end
 
 A.aggregate.fraction = mean(A.spatial.phase(:));
 A.aggregate.ssa = mean(A.spatial.edge(:));
+
+
+% This portion is necessary when importing raw data.
+if ~strcmp( ImageLocation( 1:4 ), 'http')
+    A.image = flickrGetImage( 'flickr.photos.search','Medium 640','text', sprintf('"%s"',imnm));
+    A.link = flickrGetImage( 'flickr.photos.search','Original','text', sprintf('"%s"',imnm));
+end
+
 
